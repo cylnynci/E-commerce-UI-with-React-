343b876505fb3a3bf9d6b4dad19b0ed35a3db421
cylnynci <84045109+cylnynci@users.noreply.github.com>
2022-04-02 05:02:26 +0300

    ProjeKodları

---
+++ intro/src/Navi.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import CartSummary from "./CartSummary";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from 'reactstrap'
+import { Link } from 'react-router-dom';
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar
+          color="light"
+          light expand="md"
+
+        >
+          <NavbarBrand href="/">
+            Northwind App
+          </NavbarBrand>
+          <NavbarToggler onClick={function noRefCheck() { }} />
+          <Collapse navbar>
+            <Nav
+              className="me-auto"
+              navbar
+            >
+              <NavItem>
+                <NavLink>
+                  <Link to="form1">
+                    Form Demo 1
+                  </Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink>
+                  <Link to="form2">
+                    Form Demo 2
+                  </Link>
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">
+                  GitHub
+                </NavLink>
+              </NavItem>
+              <CartSummary removeFromCart={this.props.removeFromCart} cart={this.props.cart} />
+
+            </Nav>
+
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
